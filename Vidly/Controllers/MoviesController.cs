7e86797475be1748d6fcf54139af12f5d7fe1287
Louis Mazza <louismazza@gmail.com>
2019-03-27 09:43:32 -0700

    finished section 2, about to try 2 setup site for 2 test. started navbar

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -46,7 +46,7 @@ namespace Vidly.Controllers
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12))}")]
+        [Route("movies/released/{year}/{month:regex(\\d{2}:range(1,12))}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
