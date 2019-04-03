ccc7438de5ad7e063c483e26172861588e545e9e
Louis Mazza <louismazza@gmail.com>
2019-04-03 10:33:48 -0700

    section 2 finished changes. bootstrap lumen changed to v3, still not working

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,22 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ViewResult Random()
         {
@@ -28,29 +44,6 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index()
-        {
-            var movies = new List<Movie>
-            {
-                new Movie {Name = "Golden Eye", Id = 1 },
-                new Movie {Name = "Magnolia", Id = 2 },
-                new Movie {Name = "Faster and Furiouser", Id = 3},
-            };
-
-            return View(movies);
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}:range(1,12))}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
         }
     }
 }
--- Vidly/ViewModels/RandomMovieViewModel.cs
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -11,9 +11,4 @@ namespace Vidly.ViewModels
         public Movie Movie { get; set; }
         public List<Customer> Customers { get; set; }
     }
-
-    public class MovieListModel
-    {
-        public List<Movie> Movies { get; set; }
-    }
 }
